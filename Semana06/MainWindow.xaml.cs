74de505ea021d6572449809424305a8a391e9e51
Alumno <Alumno@LAB1504-30>
2024-04-20 13:07:11 -0500

    Agregar archivos de proyecto.

---
+++ Semana06/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Semana06
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection conexion = new SqlConnection("server=LAB1504-30\\SQLEXPRESS; database=Neptuno; ");
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            RegistrarCliente registrarCliente = new RegistrarCliente();
+            registrarCliente.ShowDialog();
+
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            EliminarCliente eliminarCliente = new EliminarCliente();
+            eliminarCliente.ShowDialog();
+        }
+    }
+}
---
+++ Semana06/RegistrarCliente.xaml.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Semana06
+{
+    /// <summary>
+    /// Lógica de interacción para RegistrarCliente.xaml
+    /// </summary>
+    public partial class RegistrarCliente : Window
+    {
+        public RegistrarCliente()
+        {
+            InitializeComponent();
+        }
+        private string conexion = "Data Source=LAB1504-29\\SQLEXPRESS;Initial Catalog=Neptuno;User Id=Eduardo;Password=123456";
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            string consulta = "insert into clintes values(" + textBox1.Text + ",'" + textBox2.Text + "','" + textBox3.Text + "','" + textBox4.Text + "','" + textBox5.Text + "','" + textBox6.Text + "','" + textBox7.Text + "','" + textBox8.Text + "','" + textBox9.Text + "','" + textBox10.Text + "')";
+            SqlCommand comando = new SqlCommand(consulta);
+            comando.ExecuteNonQuery();
+            MessageBox.Show("Cliente Agregado");
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, object e)
+        {
+          
+        }
+    }
+}
